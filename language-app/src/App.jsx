8942440222f80e45f889767de969f952e1933ba2
JoshuaEyaru <56198053+JoshuaEyaru@users.noreply.github.com>
2023-03-21 09:38:10 -0700

    adding button to call new word with it's functionality

--- language-app/src/App.jsx
+++ language-app/src/App.jsx
@@ -1,33 +1,75 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [randomWord, setRandomWord] = useState('history');
+  const [currentWord, setCurrentWord] = useState('');
+  const [wordMeaning, setWordMeaning] = useState('')
+  const [prevWord, setPrevWord] = useState([])
 
+  const getWord = () => {
+    makeQuery();
+  }
+  
+  const makeQuery = () => {
+    let query = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${randomWord}`;
+    setCurrentWord(randomWord)
+    callAPI(query).catch(console.error);
+  }
+
+  const callAPI = async (query) => {
+    const response = await fetch(query);
+    const json = await response.json();
+
+    if(json[0].word === randomWord){
+      setWordMeaning(json[0].meanings)
+    }
+    
+  }
+
+  
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      
+      <div className='word-seen'>
+        <h3>Words seen</h3>
+
+        <br></br>
+        <div className='seen-display'></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className='word-new'>
+        <h2>Improve Your English Vocabulary</h2>
+        <h3>Discover English words and practice!</h3>
+        
+        <br></br>
+        <div className='display-word'>
+          <h3>{currentWord}</h3>
+          {wordMeaning.length > 0 && (
+            <ul>
+              {wordMeaning.map(meaning => (
+                <li key={meaning.partOfSpeech}>
+                  <h4>{meaning.partOfSpeech}</h4>
+                  <p>{meaning.definitions[0].definition}</p>
+                </li>
+              ))}
+            </ul>
+          )}
+
+        </div>
+
+        <br></br>
+        <button onClick={getWord}>New Word</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div className='word-banned'>
+        <h3>Banned Words</h3>
+
+        <br></br>
+        <div className='banned-display'></div>
+      </div>
+
     </div>
   )
 }
