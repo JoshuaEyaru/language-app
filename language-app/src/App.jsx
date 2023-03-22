322e46fdf214c6d0f6451001d28ea87334597e2b
JoshuaEyaru <56198053+JoshuaEyaru@users.noreply.github.com>
2023-03-21 22:19:43 -0700

    Update App.jsx

--- language-app/src/App.jsx
+++ language-app/src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
-import index from 'an-array-of-english-words';
 
 function App() {
   const [randomWord, setRandomWord] = useState('');
@@ -10,18 +8,27 @@ function App() {
   const [prevWord, setPrevWord] = useState([])
   const [bannedWords, setBannedWords] = useState([])
 
+  const wordList = ["antefixal","antefixes","anteing","antelope","ballistites","ballistospore","ballistospores","ballium","discomposedly","discomposes","discomposing","prologue","prologued","prologues","prologuing","prologuise"];
+
   const getRandomWord = () => {
     const randomIndex = Math.floor(Math.random() * wordList.length);
     const randomWord = wordList[randomIndex];
     setRandomWord(randomWord);
+    setCurrentWord('');
+    setWordMeaning('');
   }
 
+  useEffect(() => {
+    getRandomWord();
+  }, []);
+
   const getWord = () => {
+    getRandomWord();
     makeQuery();
   }
   
   const makeQuery = () => {
-    let query = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${randomWord}`;
+    let query = `https://api.dictionaryapi.dev/api/v2/entries/en/${randomWord}`;
     setCurrentWord(randomWord)
     callAPI(query).catch(console.error);
   }
@@ -29,11 +36,15 @@ function App() {
   const callAPI = async (query) => {
     const response = await fetch(query);
     const json = await response.json();
-
-    if(json[0].word === randomWord){
-      setWordMeaning(json[0].meanings)
+  
+    if(json.length === 0){
+      setWordMeaning([{partOfSpeech: '', definitions: [{definition: 'sorry api has no meaning for such word'}]}]);
+    } else if(json[0].word === randomWord){
+      setWordMeaning(json[0].meanings);
     }
   }
+  
+  
 
   const addToBannedWords = () => {
     setBannedWords([...bannedWords, currentWord]);
